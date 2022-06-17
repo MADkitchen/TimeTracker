09d615ca44bf8c17cac14e70cdbedf1e1c7c5ea5
Giovanni Cascione <ing.cascione@gmail.com>
2022-06-17 04:37:43 +0200

    move users to separate tables

--- TimeTracker.php
+++ TimeTracker.php
@@ -38,10 +38,10 @@ class TimeTracker extends \MADkitchen\Modules\Module {
 			id  bigint(20) NOT NULL AUTO_INCREMENT,
 			activity_id            bigint(20)   NOT NULL,
                         date_rec  DATE   NOT NULL,
-                        user_group            tinytext   NOT NULL,
+                        user_group            bigint(20)   NOT NULL,
                         time_units            decimal(4,2)   NOT NULL,
-                        user_name            tinytext   NOT NULL,
-                        user_role            tinytext   NOT NULL,
+                        user_name            bigint(20)   NOT NULL,
+                        user_role            bigint(20)   NOT NULL,
                         job_tag            bigint(20)   NOT NULL,
 			PRIMARY KEY (id)
 			"
@@ -82,10 +82,12 @@ class TimeTracker extends \MADkitchen\Modules\Module {
                 'user_group' => [
                     'name' => 'user_group',
                     'description' => 'User cluster',
-                    'type' => 'tinytext',
+                    'type' => 'bigint',
+                    'length' => '20',
                     'unsigned' => true,
                     'searchable' => true,
                     'sortable' => true,
+                    'relation' => 'user_group',
                 ],
                 //time_units
                 'time_units' => [
@@ -94,26 +96,28 @@ class TimeTracker extends \MADkitchen\Modules\Module {
                     'type' => 'DECIMAL',
                     'length' => '4,2',
                     'unsigned' => true,
-                //'searchable' => true,
-                //'sortable'   => true,
                 ],
                 //user_name
                 'user_name' => [
                     'name' => 'user_name',
                     'description' => 'User name',
-                    'type' => 'tinytext',
+                    'type' => 'bigint',
+                    'length' => '20',
                     'unsigned' => true,
                     'searchable' => true,
                     'sortable' => true,
+                    'relation' => 'user_name',
                 ],
                 //user_role
                 'user_role' => [
                     'name' => 'user_role',
                     'description' => 'User role',
-                    'type' => 'tinytext',
+                    'type' => 'bigint',
+                    'length' => '20',
                     'unsigned' => true,
                     'searchable' => true,
                     'sortable' => true,
+                    'relation' => 'user_role',
                 ],
                 //job_tag
                 'job_tag' => [
@@ -155,7 +159,7 @@ class TimeTracker extends \MADkitchen\Modules\Module {
                 \MADkitchen\Database\Handler::get_std_lookup_table('job_tag', "Job tag", [['tag' => 'job_wbs']]),
                 \MADkitchen\Database\Handler::get_std_lookup_table('user_group', "Group of user"),
                 \MADkitchen\Database\Handler::get_std_lookup_table('user_role', "Role of users"),
-                \MADkitchen\Database\Handler::get_std_lookup_table('user_no', "Users",[['tag' => 'user_role'],['tag' => 'user_group']]),
+                \MADkitchen\Database\Handler::get_std_lookup_table('user_name', "Users",[['tag' => 'user_role'],['tag' => 'user_group'],['tag' => 'wp_id']]),
         );
 
         parent::__construct();
