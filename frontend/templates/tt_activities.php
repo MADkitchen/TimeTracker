91f066a60a90a8950874f88f6d66e16383bb93de
Giovanni Cascione <ing.cascione@gmail.com>
2022-06-22 21:56:26 +0200

    add jobs/activities pages template files

---
+++ frontend/templates/tt_activities.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Copyright (C) 2022 Giovanni Cascione <ing.cascione@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Exit if accessed directly.
+defined('ABSPATH') || exit;
+?>
+
+<div class="w3-responsive">
+        <?php ts_build_hierarchical_table(['activity_group', 'activity_id']) ?>
+    Buttons are disabled in this test.
+</div>
